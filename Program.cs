9b5e617942a8571c46fd3504de775eb171a442bb
Ziriado <115992429+Ziriado@users.noreply.github.com>
2022-10-26 14:54:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Restaurant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
